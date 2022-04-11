e6119688ffc741935e2c333b89a988614ec92f8b
Luivatra <luivatra@gmail.com>
2022-04-11 10:09:48 +0200

    include jar

--- setup.py
+++ setup.py
@@ -5,21 +5,23 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-     name='ergo_python_appkit',  
-     version='0.0.1',
-     packages=['ergo_python_appkit'] ,
-     author="Robert Pieter van Leeuwen",
-     author_email="luivatra@gmail.com",
-     description="A python wrapper for the Ergo Appkit",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/ergo-pad/ergo-python-appkit",
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
-     install_requires=[
-          'jpype1',
-      ]
+    name='ergo_python_appkit',  
+    version='0.0.1',
+    packages=['ergo_python_appkit'] ,
+    author="Robert Pieter van Leeuwen",
+    author_email="luivatra@gmail.com",
+    description="A python wrapper for the Ergo Appkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ergo-pad/ergo-python-appkit",
+    package_data={'ergo_python_appkit': ['*.jar']},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'jpype1',
+    ]
  )
