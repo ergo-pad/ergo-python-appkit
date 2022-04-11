2999e73ef4f8f163b5f208e0a387b9b44da11aa1
Luivatra <luivatra@gmail.com>
2022-04-11 09:14:47 +0200

    setup

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setuptools.setup(
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/ergo-pad/ergo-python-appkit",
-     packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
